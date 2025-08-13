222aea93372c9b77bcd0343a8c3be217edab5df3
fxpby <fxpby@outlook.com>
2025-08-13 13:45:22 +0800

    feat: add logTitle util

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
-export function hello() {
-    console.log("Hello, world!");
+import chalk from "chalk";
+
+export function logTitle(message: string) {
+  const totalLength = 80;
+  const messageLength = message.length;
+  const padding = Math.max(0, totalLength - messageLength - 4);
+  const paddedMessage = `${"=".repeat(
+    Math.floor(padding / 2)
+  )} ${message} ${"=".repeat(Math.ceil(padding / 2))}`;
+  console.log(chalk.bold.cyanBright(paddedMessage));
 }
